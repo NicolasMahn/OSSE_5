794fffc47682a597ff17dcf6da903f6877e17f5e
Nicolas Mahn <Nicolas.Mahn@hs-furtwangen.de>
2022-04-21 13:38:05 +0200

    Initial commit

---
+++ src/main/java/de/hfu/App.java
@@ -0,0 +1,31 @@
+package de.hfu;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )  {
+
+        String s = read();
+        System.out.print(s.toUpperCase());
+    }
+
+    public static String read() {
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String s = "";
+        try {
+            s = in.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return s;
+
+    }
+}
